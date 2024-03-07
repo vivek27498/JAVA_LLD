6c98f46e535e69c39d01557bb0ba428cc3ff2496
VIVEK CHAVAN <vivek27498@gmail.com>
2024-03-07 17:51:34 +0530

    Updated folder structure

---
+++ LLD2/src/OpenClosePrinciple/Main.java
@@ -0,0 +1,12 @@
+package OpenClosePrinciple;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee pm = new PermanentEmployee(2000,"Vivek");
+        Employee tm = new TemporaryEmployee(1000,"Sakshi");
+
+        System.out.println("Vivek Bonus--"+pm.CalculateBonus(2000));
+        System.out.println("Sakshi Bonus--"+tm.CalculateBonus(2000));
+    }
+}
